026053b8bd46aedca55df403aa9f446e0ab93b78
Agustin Rosso <AgustinRosso_1@hotmail.com>
2025-07-06 23:02:21 +0200

    test

--- Tests/PoolServiceTests.cs
+++ Tests/PoolServiceTests.cs
@@ -8,11 +8,22 @@ using System;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.TestTools;
 
 namespace Rossoforge.Pool.Tests
 {
     public class PoolServiceTests
     {
+        [TearDown]
+        public void TearDown()
+        {
+            var root = GameObject.Find("PoolRoot");
+            if (root != null)
+            {
+                GameObject.DestroyImmediate(root);
+            }
+        }
+
         [Test]
         public void Initialize_CreatesRootAndEmptyPoolGroups()
         {
@@ -33,17 +44,19 @@ namespace Rossoforge.Pool.Tests
             service.Initialize();
 
             var data = Substitute.For<IPooledGameobjectData>();
-            data.name.Returns("MyPrefab");
+            data.name.Returns("MyPrefab1");
             data.MaxSize.Returns(5);
-            data.AssetReference.Returns(new GameObject("AssetRef"));
+            data.AssetReference.Returns(new GameObject("MyPrefab1"));
 
             // Act
-            var pooled = service.Get(data, null, Vector3.zero, Space.World);
+            var pooled = service.Get(data, null, new Vector3(1,2,3), Space.World);
 
             // Assert
             Assert.IsNotNull(pooled);
-            var poolObject = GameObject.Find("MyPrefab");
+            var poolObject = GameObject.Find("MyPrefab1(Clone)");
             Assert.IsNotNull(poolObject);
+
+            Assert.AreEqual(new Vector3(1, 2, 3), poolObject.transform.position, "Pooled object should be at the specified position");
         }
 
         [Test]
@@ -54,16 +67,16 @@ namespace Rossoforge.Pool.Tests
             service.Initialize();
 
             var data = Substitute.For<IPooledGameobjectData>();
-            data.name.Returns("MyPrefab");
+            data.name.Returns("MyPrefab2");
             data.MaxSize.Returns(5);
-            data.AssetReference.Returns(new GameObject("AssetRef"));
+            data.AssetReference.Returns(new GameObject("MyPrefab2"));
 
             // Act
             var pooled = service.Get<PooledObject>(data, null, Vector3.zero, Space.World);
 
             // Assert
             Assert.IsNotNull(pooled);
-            var poolObject = GameObject.Find("MyPrefab");
+            var poolObject = GameObject.Find("MyPrefab2(Clone)");
             Assert.IsNotNull(poolObject);
         }
 
@@ -74,14 +87,14 @@ namespace Rossoforge.Pool.Tests
             service.Initialize();
 
             var data = Substitute.For<IPooledGameobjectData>();
-            data.name.Returns("PreloadPrefab");
+            data.name.Returns("MyPrefab3");
             data.MaxSize.Returns(3);
-            data.AssetReference.Returns(new GameObject("AssetRef"));
+            data.AssetReference.Returns(new GameObject("MyPrefab3"));
 
             // Act
             service.Populate(data);
 
-            var poolObject = GameObject.Find("PreloadPrefab");
+            var poolObject = GameObject.Find("MyPrefab3");
             Assert.IsNotNull(poolObject);
         }
 
@@ -92,9 +105,9 @@ namespace Rossoforge.Pool.Tests
             service.Initialize();
 
             var data = Substitute.For<IPooledGameobjectData>();
-            data.name.Returns("ToClear");
+            data.name.Returns("MyPrefab4");
             data.MaxSize.Returns(1);
-            data.AssetReference.Returns(new GameObject("AssetRef"));
+            data.AssetReference.Returns(new GameObject("MyPrefab4"));
 
             var pooled = service.Get(data, null, Vector3.zero, Space.World);
             Assert.IsNotNull(pooled);
@@ -110,10 +123,11 @@ namespace Rossoforge.Pool.Tests
             service.Initialize();
 
             var data = Substitute.For<IPooledGameobjectData>();
-            data.name.Returns("ToClear");
+            data.name.Returns("MyPrefab5");
             data.MaxSize.Returns(1);
-            data.AssetReference.Returns(new GameObject("AssetRef"));
+            data.AssetReference.Returns(new GameObject("MyPrefab5"));
 
+            // Act
             bool isCleared = service.Clear(data);
             Assert.IsFalse(isCleared);
         }
@@ -127,6 +141,7 @@ namespace Rossoforge.Pool.Tests
             var root = GameObject.Find("PoolRoot");
             Assert.IsNotNull(root);
 
+            // Act
             service.Dispose();
 
             Assert.IsTrue(root == null || root.Equals(null), "Root should be destroyed");
@@ -146,15 +161,7 @@ namespace Rossoforge.Pool.Tests
             data.name.Returns("async_key");
             data.AssetReference.Returns(assetReference);
 
-            Func<NSubstitute.Core.CallInfo, Awaitable<GameObject>> value = async call =>
-                {
-                    await Awaitable.WaitForSecondsAsync(0.1f); // Simulate async loading delay
-                    return new GameObject("AsyncPrefab");
-                };
-
-            addressableService
-                .LoadAssetAsync<GameObject>(data.AssetReference)
-                .Returns(value);
+            WhenAddressableLoadAssetAsyncGameObject(addressableService, data);
 
             // Act
             var result = await service.GetAsync(data, null, Vector3.zero, Space.World);
@@ -163,23 +170,34 @@ namespace Rossoforge.Pool.Tests
             Assert.IsNotNull(GameObject.Find("AsyncPrefab"));
         }
 
-        /*
+        [Test]
+        public void GetAsync_ThrowsIfAddressableServiceIsNull()
+        {
+            var service = new PoolService();
+            service.Initialize();
 
+            var data = Substitute.For<IPooledObjectAsyncData>();
 
-            [Test]
-            public void GetAsync_ThrowsIfAddressableServiceIsNull()
+            LogAssert.Expect(LogType.Error, "Failed to load asset: AddressableService is null. Ensure it is properly registered in the service container.");
+
+            Assert.ThrowsAsync<NullReferenceException>(async () =>
             {
-                var service = new PoolService();
-                service.Initialize();
+                await service.GetAsync(data, null, Vector3.zero, Space.World);
+            });
+        }
 
-                var data = Substitute.For<IPooledObjectAsyncData>();
-                data.AssetReference.Returns("async_key");
 
-                Assert.ThrowsAsync<NullReferenceException>(async () =>
-                {
-                    await service.GetAsync(data, null, Vector3.zero, Space.World);
-                });
-            }
-            */
+        private static void WhenAddressableLoadAssetAsyncGameObject(IAddressableService addressableService, IPooledObjectAsyncData data)
+        {
+            Func<NSubstitute.Core.CallInfo, Awaitable<GameObject>> value = async call =>
+            {
+                await Awaitable.WaitForSecondsAsync(0.1f); // Simulate async loading delay
+                return new GameObject("AsyncPrefab");
+            };
+
+            addressableService
+                .LoadAssetAsync<GameObject>(data.AssetReference)
+                .Returns(value);
+        }
     }
 }
